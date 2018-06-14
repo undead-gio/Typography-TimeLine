7006db4e6f9d0f297cdbbf48cfe9a1309cf45b57
undead-gio <gio.com@hotmail.it>
2018-06-14 14:43:10 +0200

    progetto terminato

---
+++ index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+	setTimeout(function(){
+
+
+	$('.load').animate({
+	opacity:0
+	}, 2000, function(){
+		$(".load").css({"display":"none"})
+	});
+
+
+	}, 1000)
+})
